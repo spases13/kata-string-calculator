c1e51aa4c85495b044004f9e1acf3426a893f641
Yassine BIZNAOUNE <yassine.biznaoune@e-polytechnique.com>
2024-02-20 10:51:21 +0100

    question1+test

---
+++ src/test/java/fr/norsys/stringcalculator/ClazzTest.java
@@ -0,0 +1,34 @@
+package fr.norsys.stringcalculator;
+
+/**
+ * ClazzTest
+ */
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class ClazzTest {
+
+    @Test
+    public void checkAddWithEmptyString() {
+        Clazz c = new Clazz();
+        int result = c.Add("");
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void checkAddWithSingleNumber() {
+        Clazz c = new Clazz();
+        int result = c.Add("5");
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void checkAddWithTwoNumbers() {
+        Clazz c = new Clazz();
+        int result = c.Add("3,7");
+        assertEquals(10, result);
+    }
+
+
+}
